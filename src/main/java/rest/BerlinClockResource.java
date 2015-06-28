2bdb85d894094ea0275f56f38142094dd82129ed
Anca Sarb <asarb@mdsol.com>
2015-06-28 19:20:48 +0100

    remove date formatter

--- src/main/java/rest/BerlinClockResource.java
+++ src/main/java/rest/BerlinClockResource.java
@@ -1,19 +1,4 @@
 package rest;
 
-import berlinclock.BerlinClock;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
-
-@Path("/berlin-clock")
-@Produces(MediaType.TEXT_PLAIN)
 public class BerlinClockResource {
-
-    @GET
-    public String getBerlinTime(@QueryParam("time") String time) {
-        return new BerlinClock().getTime(time);
-    }
 }
