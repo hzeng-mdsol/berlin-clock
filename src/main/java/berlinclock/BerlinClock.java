b86e144c11582a9b0e0b929db4416a3a91593455
Anca Sarb <asarb@mdsol.com>
2015-06-28 20:14:00 +0100

    cleanup

--- src/main/java/berlinclock/BerlinClock.java
+++ src/main/java/berlinclock/BerlinClock.java
@@ -5,7 +5,6 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class BerlinClock {
-
     public String getTime(String time) {
         BerlinTimeCalculator calculator = new BerlinTimeCalculator(LocalTime.parse(time));
 
@@ -16,5 +15,5 @@ public class BerlinClock {
                             calculator.calculateOneMinuteLamps())
                     .map(lamps -> lamps.map(BerlinLamp::getLamp).collect(Collectors.joining()))
                     .collect(Collectors.joining("\n"));
-}
+    }
 }
