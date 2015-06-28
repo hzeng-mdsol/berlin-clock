5aa630fc523fb4f69d2597ac315fa9e0df757fb9
Anca Sarb <asarb@mdsol.com>
2015-06-28 19:59:36 +0100

    - add dropwizard unit test

--- src/main/java/rest/BerlinClockResource.java
+++ src/main/java/rest/BerlinClockResource.java
@@ -1,21 +1,18 @@
 package rest;
 
 import berlinclock.BerlinClock;
-import com.google.common.base.Optional;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import java.time.LocalTime;
 
 @Path("/berlin-clock")
 @Produces(MediaType.TEXT_PLAIN)
 public class BerlinClockResource {
-
     @GET
-    public String getBerlinTime(@QueryParam("time") Optional<String> time) {
-        return new BerlinClock().getTime(time.or(LocalTime.now().toString()));
+    public String getBerlinTime(@QueryParam("time") String time) {
+        return new BerlinClock().getTime(time);
     }
 }
---
+++ src/test/java/rest/BerlinClockResourceTest.java
@@ -0,0 +1,32 @@
+package rest;
+
+import berlinclock.BerlinClock;
+import io.dropwizard.testing.junit.ResourceTestRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class BerlinClockResourceTest {
+    @Rule
+    public final ResourceTestRule resources = ResourceTestRule.builder()
+                                                                .addResource(new BerlinClockResource())
+                                                                .build();
+
+    @Test
+    public void testGetBerlinTime() {
+        String timeParam = "18:03:01";
+        String berlinTime = "O\nRRRO\nRRRO\nOOOOOOOOOOO\nYYYO";
+
+        BerlinClock berlinClock = mock(BerlinClock.class);
+        when(berlinClock.getTime(timeParam)).thenReturn(berlinTime);
+
+        assertThat(resources.client().target("/berlin-clock")
+                                     .queryParam("time", timeParam)
+                                     .request()
+                                     .get(String.class))
+                .isEqualTo(berlinTime);
+    }
+}
