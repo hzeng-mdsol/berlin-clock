2bd40d82a02171b380861a01602c5858fdff6b4e
Anca Sarb <asarb@mdsol.com>
2015-06-28 19:23:10 +0100

    add basic rest service with dropwizard

--- src/main/java/rest/BerlinClockResource.java
+++ src/main/java/rest/BerlinClockResource.java
@@ -1,4 +1,21 @@
 package rest;
 
+import berlinclock.BerlinClock;
+import com.google.common.base.Optional;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.time.LocalTime;
+
+@Path("/berlin-clock")
+@Produces(MediaType.TEXT_PLAIN)
 public class BerlinClockResource {
+
+    @GET
+    public String getBerlinTime(@QueryParam("time") Optional<String> time) {
+        return new BerlinClock().getTime(time.or(LocalTime.now().toString()));
+    }
 }
